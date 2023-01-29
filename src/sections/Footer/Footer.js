e52f07576be2f713106cfb6725be0072bea2d87c
abdel fatah achouri <fatehachouri@outlook.fr>
2023-01-29 14:14:23 +0100

    last feautures

--- src/sections/Footer/Footer.js
+++ src/sections/Footer/Footer.js
@@ -6,7 +6,7 @@ const Footer = () => {
     <div className='row'>
       <div className='col-lg-12'>
         <p>Copyright © 2036 <a href='/#'>Cyborg Gaming </a>Company. All rights reserved.<br />
-Design:<a href='tepmlatemo.com' target='_blank'>Achouri Fateh</a>  </p>
+Design:<a href='tepmlatemo.com' target='_blank' rel='noreferrer'>Achouri Fateh</a>  </p>
 
       </div>
     </div>
